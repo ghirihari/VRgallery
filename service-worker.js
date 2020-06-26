abc7fdf4cb48c913f392ef86f46036c2676d2a5b
ghirihari <ghirihari@gmail.com>
2020-06-26 21:23:15 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/VRgallery/precache-manifest.8608a986ec5c181b655276e7175c31c7.js"
+  "/VRgallery/precache-manifest.cbae4dbb440633533bb1f5000fe0622c.js"
 );
 
 self.addEventListener('message', (event) => {
